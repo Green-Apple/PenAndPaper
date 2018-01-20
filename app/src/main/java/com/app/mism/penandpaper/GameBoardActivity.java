7ca7e7fce8f74889de3dd1128a225f4043b2d74d
Matthew Meelhuysen <matthew.meelhuysen@gmail.com>
2018-01-20 17:16:51 -0600

    Added empty game board activity, play button on main menu takes you to GameBoardActivity

---
+++ app/src/main/java/com/app/mism/penandpaper/GameBoardActivity.java
@@ -0,0 +1,14 @@
+package com.app.mism.penandpaper;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameBoardActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_board);
+    }
+}
--- app/src/main/java/com/app/mism/penandpaper/MainActivity.java
+++ app/src/main/java/com/app/mism/penandpaper/MainActivity.java
@@ -1,8 +1,11 @@
 package com.app.mism.penandpaper;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void onClickButton(View v){
+        Toast.makeText(this, "Clicked on button", Toast.LENGTH_LONG).show();
+
+        Intent intent = new Intent(this, GameBoardActivity.class);
+        startActivity(intent);
     }
 }
